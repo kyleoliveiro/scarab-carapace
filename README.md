13664bb7ec6aea3a93b5894a12fda93dc1aed1bb
Kyle Oliveiro <kyle@watchtower.digital>
2016-06-10 20:53:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Carapace
+
+[![npm](https://img.shields.io/npm/v/scarab-carapace.svg)](https://www.npmjs.com/package/scarab-scss) [![Build Status](https://travis-ci.org/watchtowerdigital/carapace.svg?branch=master)](https://travis-ci.org/watchtowerdigital/carapace) 
+
+
+
+## Sass UI framework for rapid prototying
+
+Carapace is an extension of the [Scarab utility framework](https://github.com/watchtowerdigital/scarab.git).
+
+
+
+## Table of contents
+* [**Installation**](#installation)
+* [**Features**](#features)
+	* [**Scarab framework**](#scarab-framework)
+	* [**Object-oriented approach**](#object-oriented-approach)
+	* [**Responsive by default**](#responsive-by-default)
+	* [**Vertical rythmn**](#vertical-rythmn)
+	* [**Automatic typesetting**](#automatic-typesetting)
+	* [**Layout & helper classes**](#layout-helper-classes)
+	* [**Component inventory**](#component-inventory)
+* [**Documentation**](#documentation)
+* [**Resources**](#resources)
+
+
+
+## Installation
+
+To get started, add Carapace as a dependency in your project via npm:
+
+```
+npm install scarab-carapace --save-dev
+```
+
+Import `carapace.scss` at the beginning of your stylesheet:
+
+```scss
+@import "path/to/node_modules/scarab-carapace/scss/carapace";
+```
+
+Importing Carapace also imports the Scarab framework, which in turn creates a new global variable, `$SCARAB` in your Sass project.
+
+For more information on configuring Scarab and Carapace, refer to [Scarab: Configuration](https://github.com/watchtowerdigital/scarab#configuration) and the official Configuration section of Carapace's documentation.
+
+
+
+## Features
+
+### Scarab framework
+Carapace imports the the [Scarab utility framework](https://github.com/watchtowerdigital/scarab.git). Scarab's functions and mixins are made available with Carapace.
+
+### Object-oriented approach
+Designed with an object-oriented approach in mind. Carapace kickstarts your projects and helps you write scalable and maintainable stylesheets.
+
+### Responsive by default
+Everything is designed for mobile first. Use breakpoint hooks which are available on some helper and layout classes to fine-tune your interface at different breakpoints.
+
+### Vertical rythmn
+Achieve vertical rythmn by default. Use the `bl()` and `type-scale()` functions and mixins to ensure consistent spacings throughout your interface.
+
+### Automatic typesetting
+Scarab includes styles to apply a consistent visual heirarchy to typographical elements.
+
+### Layout and helper classes
+Included are layout classes which help with rapid prototyping, and helper classes which overwrite specific styles on an element.
+
+### Component inventory
+Carapace provides a convenient method for managing the UI "components" for your application.
+
+
+
+## Documentation
+
+Documentation is in development.
+
+
+
+## Resources
+
+* [**scarab-snippets**](https://github.com/watchtowerdigital/scarab-snippets.git) — Sublime Text snippets for the Scarab Sass utility framework
---
+++ gulpfile.babel.js
@@ -0,0 +1,187 @@
+'use strict';
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import mqpacker from 'css-mqpacker';
+import { create as bsCreate } from 'browser-sync';
+import { exec as exec } from 'child_process';
+
+
+const browserSync = bsCreate();
+const $ = gulpLoadPlugins();
+
+const dirs = {
+	src: './scss/',
+	dest: './build/'
+};
+
+const paths = {
+	sass: {
+		main: 'carapace.scss',
+		src: dirs.src,
+		dest: dirs.dest,
+		ext: '.+(scss|css)'
+	},
+	styleguide: {
+		src: dirs.src,
+		dest: './styleguide/',
+		ext: '.+(html|md)'
+	},
+	test: {
+		dest: dirs.dest
+	}
+};
+
+const config = {
+	sass: {
+		outputStyle: 'expanded'
+	},
+	autoprefixer: {
+		browsers: ['last 2 versions']
+	},
+	mqpacker: {
+		sort: true
+	}
+};
+
+
+
+/* 
+* `gulp sass`
+* -------------
+* – Glob import .scss files
+* - Compile Sass to CSS
+* - Apply PostCSS transforms
+* - Generate minified CSS file
+*/
+
+gulp.task('sass', () => {
+
+	return gulp.src( paths.sass.src + paths.sass.main )
+
+		.pipe( $.sass( config.sass )
+			.on('error', $.sass.logError) )
+
+		.pipe( $.postcss([
+
+			autoprefixer( config.autoprefixer ),
+			mqpacker( config.mqpacker )
+
+		]) )
+
+	.pipe( gulp.dest( paths.sass.dest ) )
+
+		.pipe( $.postcss([
+			cssnano()
+		]) )
+
+		.pipe( $.rename( (path) => {
+			path.extname = '.min.css'
+		}) )
+
+	.pipe( $.size({ title: 'minified' }) )
+	.pipe( $.size({ title: 'minified, gzipped', gzip: true }) )
+	
+	.pipe( gulp.dest( paths.sass.dest ) )
+
+});
+
+
+
+/* 
+* `gulp styleguide`
+* ------------
+* – Build KSS documentation
+*/
+
+gulp.task('styleguide', ['sass'], () => {
+	gulp.src( paths.sass.dest + 'carapace.min.css' )
+	.pipe( gulp.dest( paths.styleguide.dest ) );
+
+	gulp.src( './styleguide-theme/styleguide.js' )
+	.pipe( gulp.dest( paths.styleguide.dest ) );
+
+	gulp.src( './styleguide-theme/styleguide.scss' )
+		.pipe( $.sass( config.sass )
+			.on('error', $.sass.logError) )
+
+		.pipe( $.postcss([
+
+			autoprefixer( config.autoprefixer ),
+			mqpacker( config.mqpacker )
+
+		]) )
+
+		.pipe( $.postcss([
+			cssnano()
+		]) )
+
+		.pipe( $.rename( (path) => {
+			path.extname = '.min.css'
+		}) )
+			
+	.pipe( gulp.dest( paths.styleguide.dest ) );
+
+	return exec('npm run kss', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+	});
+});
+
+
+/* 
+* `gulp watch`
+* ------------
+* – Watch source files for changes
+* - Run relevant tasks on change
+*/
+
+gulp.task('watch', () => {
+
+	var _glob = ( resource ) => {
+		return paths[resource].src + '**/*' + paths[resource].ext;
+	}
+
+	$.watch([ _glob('sass')], ( file ) => {
+		gulp.start('styleguide');
+	});
+
+	$.watch([ _glob('styleguide')], ( file ) => {
+		gulp.start('styleguide');
+	});
+
+});
+
+
+
+/* 
+* `gulp serve`
+* ------------
+* – Serve build directories
+*/
+
+gulp.task('serve', () => {
+
+	browserSync.init({
+		server: {
+			baseDir: paths.styleguide.dest
+		}
+	});
+
+	gulp.watch( dirs.dest + '/**/*' ).on( 'change', () => {
+		browserSync.reload();
+	});
+
+});
+
+
+
+/* 
+* `gulp`
+* ------
+* – Run `gulp sass`
+*/
+
+gulp.task('default', ['styleguide']);
